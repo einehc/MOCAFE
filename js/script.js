d9099ac6e67da311afacf7b96849310567944955
einehc <ningas.chenie@gmail.com>
2022-12-05 17:24:28 +0800

    add testimonials and login page

--- js/script.js
+++ js/script.js
@@ -11,3 +11,37 @@ document.onclick = function(clickEvent){
         header.classList.remove ('active');
     }
 }
+
+
+
+//TESTIMONIALS SECTION
+
+var swiper = new Swiper(".slideContent", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
